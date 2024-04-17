f3b84cb2b01bff51942acd1fa093e564889a69c7
joaquinbian <joacobian21@gmail.com>
2024-04-16 22:52:58 -0300

    ejercicio 2

---
+++ ejercicio-2/scale-3.cpp
@@ -0,0 +1,20 @@
+//importamos libreria para hacer print e ingresar info 
+#include <iostream>
+
+//usamos el namespace
+using namespace std;
+/*
+Hacer un programa para mostrar por pantalla los números del 1 al 20 salteando de a 3 valores. Es decir: 1, 4, 7, 10, 13, 16, 19.
+Importante: El programa no tiene ningún ingreso de datos.
+
+*/
+int main(){
+ 
+
+    for(int i = 1; i <= 20; i += 3){
+
+        cout << i << endl;
+    }
+
+    return 0;
+}
