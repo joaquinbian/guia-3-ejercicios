4ac1cd8aa6c6a976efbc7a6ea195db7aded1a503
joaquinbian <joacobian21@gmail.com>
2024-04-16 22:48:15 -0300

    ejercicio 1

---
+++ template.cpp
@@ -0,0 +1,13 @@
+//importamos libreria para hacer print e ingresar info 
+#include <iostream>
+
+//usamos el namespace
+using namespace std;
+
+int main(){
+ 
+
+    //esto es por buena practica, si el programa
+    //termina exitosamente, retorna 0
+    return 0;
+}
